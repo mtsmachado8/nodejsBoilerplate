dd739595304e0a0454f56cb221861e44a01c2c00
mtsmachado8 <mtsmachado8@gmail.com>
2019-08-28 09:56:29 -0300

    a

--- packages/infra/index.js
+++ packages/infra/index.js
@@ -3,6 +3,3 @@ console.log('changed some things');
 console.log('changed some things');
 console.log('changed some things');
 console.log('changed some things');
-console.log('changed some things');
-console.log('changed some things');
-console.log('changed some things');
