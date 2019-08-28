e3baf9f014dc2e2067a12103303b3bd5d9d01a98
mtsmachado8 <mtsmachado8@gmail.com>
2019-08-28 09:43:59 -0300

    feat(infra): new version

--- packages/infra/index.js
+++ packages/infra/index.js
@@ -2,3 +2,5 @@ console.log('changed some things');
 console.log('changed some things');
 console.log('changed some things');
 console.log('changed some things');
+console.log('changed some things');
+console.log('changed some things');
